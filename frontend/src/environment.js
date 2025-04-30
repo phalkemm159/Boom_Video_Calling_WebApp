3958c1729e56fc93e8671f184ef1280aa7dd5a0d
Mahesh Phalke <161316527+phalkemm159@users.noreply.github.com>
2025-05-01 04:14:01 +0530

    Update environment.js

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://boom-video-call.onrender.com" :
+    "https://boom-video-calling-webapp-backend.onrender.com" :
     "http://localhost:8000"
 export default server;
